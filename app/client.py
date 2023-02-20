4509668c7a879e00e215b1664d5aafda89d6f744
Brian Ahern <bahern@8west.ie>
2023-02-20 17:06:10 +0000

    remove unused code

--- app/client.py
+++
@@ -1,2 +0,0 @@
-import app
-app.print_hello_world()
---
+++ app/server.py
@@ -0,0 +1,9 @@
+'''Server code'''
+
+
+def print_hello_world():
+    '''Prints a hello world message'''
+    print("hello world!")
+
+
+print_hello_world()
