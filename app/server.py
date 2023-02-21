ca9cc89df06473203dbc888b7d2c4d955882076b
Brian Ahern <bahern@8west.ie>
2023-02-21 09:37:08 +0000

    add a module for calculations and import it to server.py

--- app/server.py
+++ app/server.py
@@ -1,9 +1,16 @@
 '''Server code'''
 
+import calculator
+
 
 def print_hello_world():
     '''Prints a hello world message'''
     print("hello world!")
 
+
+print(calculator.add_numbers("brian", "Ahern"))
+print(calculator.add_numbers(9, 8, 7, 6, 5, 4, 3, 2, 1))
+
+print(calculator.multiply_two_numbers(5, 5))
 
 print_hello_world()
